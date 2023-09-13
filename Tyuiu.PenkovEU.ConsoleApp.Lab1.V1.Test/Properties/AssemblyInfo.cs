816e215fcd98f5b1228099d36322bd0d9bf2d947
PenkovEugene <penkovzheny@gmail.com>
2023-09-14 00:08:10 +0500

    Исправлены недочёты в титульном листе

---
+++ Tyuiu.PenkovEU.ConsoleApp.Lab1.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PenkovEU.ConsoleApp.Lab1.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PenkovEU.ConsoleApp.Lab1.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad67ef49-478c-43da-a2b0-9cd70ea931bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
