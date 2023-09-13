7dcddad324c02e68c77dbcb9126392b11be168cb
Eugene <144609566+PenkovEugene@users.noreply.github.com>
2023-09-13 22:18:00 +0500

    Add files via upload

---
+++ Lab1/Tyuiu.PenkovEU.ConsoleApp.Lab1.V1.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tyuiu.PenkovEU.ConsoleApp.Lab1.V1.Lib
+{
+    public class DataService
+    {
+        public double SolveExpressV_1_1(double x, double y, double a)
+        {
+            double result = ((3 + x / y) / ((2 * a) / x)) - ((3 * x * a + 2 * a + a * y) / (3 * a + 2 * x - y)) + (10 * y * a);
+            return result;
+        }
+
+        public double SolveExpressV_1_2(double x, double y, double a)
+        {
+            double result = (3 * x) + ((3 * x + y - 4 * a) / (a + 2 * x + 7 * y)) + 5 + (((a / y) + 1) / ((2 * a) / (x)));
+            return result;
+        }
+
+        public double SolveExpressV_5_1(double x, double y, double a)
+        {
+            double result = 6 * y - ((a + 3 / x) / ((2 - x * a) / (x * y))) + ((2 + 3 * a) / (10 * x + 3 * a)) + 4 * y;
+            return result;
+        }
+
+        public double SolveExpressV_5_2(double x, double y, double a)
+        {
+            double result = 2 * y + ((2 * 3 * a) / (10 * x - 3 * a)) + 2 * a + ((a / y) / (3 * a / x));
+            return result;
+        }
+    }
+}
---
+++ Lab1/Tyuiu.PenkovEU.ConsoleApp.Lab1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PenkovEU.ConsoleApp.Lab1.V1.Lib;
+
+namespace Tyuiu.PenkovEU.ConsoleApp.Lab1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpressionV_1_1()
+        {
+            double x = 5;
+            double y = 6;
+            double a = 9;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_1_1(x, y, a);
+
+            result = Math.Round(result, 6);
+            double wait = 534.387395;
+
+            Assert.AreEqual(result, wait);
+
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_1_2()
+        {
+            double x = 4;
+            double y = -3;
+            double a = 7;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_1_2(x, y, a);
+
+            result = Math.Round(result, 8);
+            double wait = 19.78571429;
+
+            Assert.AreEqual(result, wait);
+
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_5_1()
+        {
+            double x = 5;
+            double y = 15;
+            double a = 45;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_5_1(x, y, a);
+
+            result = Math.Round(result, 7);
+            double wait = 166.0768634;
+
+            Assert.AreEqual(result, wait);
+
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_5_2()
+        {
+            double x = 5;
+            double y = 15;
+            double a = 45;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_5_2(x, y, a);
+
+            result = Math.Round(result, 7);
+            double wait = 116.9346405;
+
+            Assert.AreEqual(result, wait);
+
+        }
+    }
+}
